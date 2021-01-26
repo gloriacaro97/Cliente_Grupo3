649fc91fcd53af0bba61afc154760d4b488f437d
MiriamVR <miriamvega.rom@gmail.com>
2021-01-26 15:29:42 +0100

    Añadidos form Inicio Sesión, Playlist, Suscripción y funcionalidad

--- codigo.js
+++ codigo.js
@@ -2,3 +2,33 @@
 
 // Principal
 var oSpotify = new Spotify();
+
+// FORMULARIOS ----------------------------------------------------------------------------------------------------------------------------------
+
+// ocultarFormularios(); 
+ocultarFormularios();
+
+// Oculta los formularios por defecto -----------------------------------------------------------------------------------------------
+function ocultarFormularios() {
+    formInicioSesión.style.display = "none";
+    formSuscripcion.style.display = "none";
+    formCrearPlaylist.style.display = "none";
+}
+
+// Muestra el formulario de Inicio de Sesión -----------------------------------------------------------------------------------------------
+function mostrarFormInicioSesion() {
+    ocultarFormularios();
+    formInicioSesión.style.display = "block";
+}
+
+// Muestra el formulario de Suscripción -----------------------------------------------------------------------------------------------
+function mostrarFormSuscripcion() {
+    ocultarFormularios();
+    formSuscripcion.style.display = "block";
+}
+
+// Muestra el formulario de Crear Playlist -----------------------------------------------------------------------------------------------
+function mostrarFormCrearPlaylist() {
+    ocultarFormularios();
+    formCrearPlaylist.style.display = "block";
+}
